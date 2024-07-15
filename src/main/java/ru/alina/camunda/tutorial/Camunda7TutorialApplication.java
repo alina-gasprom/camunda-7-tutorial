288d635530e5f69e647c8007b11470891c34f261
alina-gasprom <alina.k.91@icloud.com>
2024-07-15 16:39:58 +0300

    start

---
+++ src/main/java/ru/alina/camunda/tutorial/Camunda7TutorialApplication.java
@@ -0,0 +1,13 @@
+package ru.alina.camunda.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Camunda7TutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Camunda7TutorialApplication.class, args);
+    }
+
+}
